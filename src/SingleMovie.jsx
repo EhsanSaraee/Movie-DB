4cfa56438ee80b96a5e9abda66ba35710ef1b418
ehsan <ehsansaraee6570@yahoo.com>
2022-01-18 12:26:49 +0330

    loading every single movie details

--- src/SingleMovie.jsx
+++ src/SingleMovie.jsx
@@ -1,5 +1,61 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { API_ENDPOINT } from './context';
+
 const SingleMovie = () => {
-   return <h2>movie component</h2>;
+   const { id } = useParams();
+   const [movie, setMovie] = useState({});
+   const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState({ show: false, message: '' });
+
+   const fetchMovie = async (url) => {
+      try {
+         const { data } = await axios.get(url);
+         if (data.Response === 'False') {
+            setError({ show: true, message: data.Error });
+            setIsLoading(false);
+         } else {
+            setMovie(data);
+            setIsLoading(false);
+         }
+         setIsLoading(false);
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   useEffect(() => {
+      fetchMovie(`${API_ENDPOINT}&i=${id}`);
+   }, [id]);
+
+   if (isLoading) {
+      return <div className="loading"></div>;
+   }
+   if (error.show) {
+      return (
+         <div className="page-error">
+            <h1>{error.message}</h1>
+            <Link to="/" className="btn">
+               Back to Movie List
+            </Link>
+         </div>
+      );
+   }
+   const { Plot: plot, Poster: poster, Title: title, Year: year } = movie;
+   return (
+      <section className="single-movie">
+         <img src={poster} alt={title} />
+         <div className="single-movie-info">
+            <h2>{title}</h2>
+            <p>{plot}</p>
+            <h4>{year}</h4>
+            <Link to="/" className="btn">
+               Back to Movie List
+            </Link>
+         </div>
+      </section>
+   );
 };
 
 export default SingleMovie;
